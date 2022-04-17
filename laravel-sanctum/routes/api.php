714b5848941bac2a72e1306e5b93dbf5d92c84d6
Matheus <matheus.oliveira110101@gmail.com>
2022-04-17 10:56:14 -0300

    api funcionando

--- laravel-sanctum/routes/api.php
+++ laravel-sanctum/routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 /*
 |--------------------------------------------------------------------------
